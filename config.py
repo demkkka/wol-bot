0967553b664ca202b07cf0699c51bfd41f9fd8b8
demkkka <demka7936@gmail.com>
2022-05-24 10:32:31 +0300

    v0.1

---
+++ config.py
@@ -0,0 +1,8 @@
+botToken = 'xyzxyzxyzxyzxyzxyzxyzxyzxyzxyzxyzxyz'
+telegramUsername = 'tglogin'
+winPcMac = 'AA:AA:AA:AA:AA:A'
+winPcIp = '192.168.1.100'
+winPcUsername = 'username'
+winPcPassword = 'password'
+sshPort = '22'
+
---
+++ main.py
@@ -0,0 +1,68 @@
+import paramiko
+import subprocess
+from time import sleep
+from ping3 import ping
+from telebot import telebot, types
+from config import botToken, telegramUsername, winPcMac, winPcUsername, winPcIp, winPcPassword, sshPort
+
+bot = telebot.TeleBot(botToken)
+
+def actions_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    turnOnPcButton = types.KeyboardButton(text="turn on")
+    turnOffPcButton = types.KeyboardButton(text="turn off")
+    statusButton = types.KeyboardButton(text="status")
+    keyboard.add(statusButton, turnOnPcButton, turnOffPcButton)
+    return keyboard
+
+def statusOfPc(host):
+    resp = ping(host)
+    print(resp)
+    if resp is None:
+        return 'computer is off'
+    else:
+        return 'computer is on'
+
+def turnOnPc(message):
+    process = subprocess.run(["wakeonlan", winPcMac], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    answer = process.stdout
+    bot.send_message(message.from_user.id, answer, reply_markup=actions_keyboard())
+    sleep(25)
+    bot.send_message(message.from_user.id, statusOfPc(winPcIp), reply_markup=actions_keyboard())
+
+def turnOffWinPc(message):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=winPcIp, username=winPcUsername, password=winPcPassword, port=sshPort)
+    client.exec_command('shutdown /s /f /t 0')
+    client.close()
+    
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    if message.from_user.username == telegramUsername:
+        bot.reply_to(message, f'hi, '+message.from_user.first_name)
+    else:
+        bot.reply_to(message, f' my parents don\'t allow me to talk to strangers, we won\'t have a dialogue :<')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.from_user.username == telegramUsername:
+        if message.text.lower() == 'turn on':
+            turnOnPc(message)
+        elif message.text.lower() == 'turn off':
+            bot.send_message(message.from_user.id, 'computer is shuting down...', reply_markup=actions_keyboard())
+            turnOffWinPc(message)
+            sleep(21)
+            bot.send_message(message.from_user.id, statusOfPc(winPcIp), reply_markup=actions_keyboard())
+        elif message.text.lower() == 'status':
+            bot.send_message(message.from_user.id, statusOfPc(winPcIp), reply_markup=actions_keyboard())
+        else:
+            bot.send_message(message.from_user.id, 'I don\'t understand what this means')
+    else:
+        bot.reply_to(message, f'You\'re not my parent')
+
+def main():
+    bot.polling(none_stop=True)
+
+if __name__ == '__main__':
+    main()
